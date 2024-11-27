e145aa47f102950a9e5b7f65c0dfd2c32216bf18
SunMoon23[200~git config --global user.email gunayhsynli2003@gmail.com <gunayhsynli23@gmail.com>
2024-11-27 15:00:19 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+1// let name= "my name is Gunay"
+// console.log(name)
+
+
+2// let fullName="Gunay Huseynli"
+// console.log(fullName)
+
+
+3// person, $add
+
+
+4// let bookPrice=750;
+// let bookAmount=14;
+// let totalPrice = bookAmount * bookPrice;
+// console.log("Butun kitablarin deyeri :" , totalPrice, "AZN")
+
+
+5// let kmhSpeed=75;
+// let msSpeed= kmhSpeed * 1000 / 3600;
+
+// console.log("msSpeed", msSpeed)
+
+
+6// let bdYear = prompt("Dogum ilinizi daxil edin!");
+// let age = 2024 - bdYear;
+// let days = age * 365 ;
+// console.log("Sizin yasadiginiz gunlerin sayi" ,days);
+
+
+7// let num = 25;
+// let power = Math.pow(num, 5);
+// console.log(`${num} ədədinin beşinci dərəcə qüvvəti: ${power}`);
+
+// let remainder = num % 3;
+// console.log(`${num} ədədinin 3-ə bölünmədən qalığı: ${remainder}`);
+
+
+8// let firstName = 'Alan';
+// let lastName = 'Turing';
+// console.log(`Sizin adınız ${firstName} ${lastName}. Tanışlığımıza çox şadam!`);
+
+
+9// let firstName = 'Gunay';
+// let lastName = 'Huseynli';
+// console.log(`Sizi bir daha görməyimizə şadıq, ${firstName} ${lastName} !`);
+
+
+10// let fullName = "Gunay Huseynli"
+// let letterCount = fullName.replace(/\s/g, "").length;
+// console.log(`Ad və soyaddakı hərflərin sayı: ${letterCount}`);
+
+
+11// let fullName = "Gunay Huseynli"
+// console.log(fullName[0]);
+// console.log(fullName[6]);
+
+
+
+12// let fullName = prompt("Tam adınızı 'AD SOYAD' formatında daxil edin:");
+// let [firstName, lastName] = fullName.split(" ");
+// console.log(`Tam adınız: ${lastName} ${firstName}`);
+
+
+
+13// let name = prompt("Adınızı daxil edin:");
+// console.log(`Salam, ${name}!`);
+
+
+
+14// let num1 = parseFloat(prompt("Birinci ədədi daxil edin:"));
+// let num2 = parseFloat(prompt("İkinci ədədi daxil edin:"));
+
+// console.log(`Toplama: ${num1 + num2}`);
+// console.log(`Çıxma: ${num1 - num2}`);
+// console.log(`Vurma: ${num1 * num2}`);
+// console.log(`Bölmə: ${num1 / num2}`);
+
+
+
+15// let num1 = 12345;
+// let num2 = 98765;
+// let product = num1 * num2;
+// let sixthDigit = product.toString()[5];
+// console.log(`Hasilin 6-cı rəqəmi: ${sixthDigit}`);
+
+
